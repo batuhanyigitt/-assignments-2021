f87de9587df66a96d869610bcf342b80cba0bbce
Batuhan Yiğit <70327837+batuhanyigitt@users.noreply.github.com>
2022-01-30 23:21:52 +0100

    Add files via upload

---
+++ Assignment5/Assignment5/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assigment5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dateNow = DateTime.UtcNow.ToString("yyyy-MM-dd");
+            string fileName = dateNow + ".txt";
+
+            string ThisSave = System.IO.Path.Combine(@"./", fileName);
+
+            List<string> captainsLog = new List<string>();
+            Console.WriteLine("Write the text this area : ");
+            string userInput;
+
+            bool typingnow = true;
+
+            while (typingnow != false)
+            {
+                userInput = Console.ReadLine();
+                if (userInput == "start")
+                {
+                    do
+                    {
+                        userInput = Console.ReadLine();
+                        if (userInput != "stop")
+                        {
+                            captainsLog.Add(userInput);
+                        }
+                    } while (userInput != "stop");
+
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(ThisSave))
+                    {
+                        file.WriteLine("Captains's log \nStardate " + dateNow + "\n");
+                        foreach (var allwewant in captainsLog)
+                        {
+                            file.WriteLine(allwewant);
+                        }
+                        file.WriteLine("\n" + "Jean-Luc Picard");
+                    }
+
+                    if (userInput == "stop")
+                    {
+                        typingnow = false;
+                    }
+                }
+            }
+        }
+    }
+}
